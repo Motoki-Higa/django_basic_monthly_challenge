56db6f0d90b185c18c5f4d62c6737cc88ff325c1
Motoki Higa <mokkyhiga@gmail.com>
2022-10-18 12:06:01 +0900

    feat: includes partial template

--- challenges/urls.py
+++ challenges/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index), # /challenge/
+    path('', views.index, name='index'), # /challenge/
     path('<int:month>', views.monthly_challenge_by_number),
     path("<str:month>", views.monthly_challenge, name='month-challenge')
 ]
